08ff7eeddefac25938416c805dc1004363e83825
boduyskibidi <duyminicpx@gmail.com>
2026-05-18 23:52:10 +0700

    Add files via upload

---
+++ userscript.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         ANAKUxLOVANLEO
+// @namespace    ANAKU
+// @version      1.0
+// @match        *://linkhuongdan.online/*
+// @grant        none
+// @run-at       document-end
+// @require      https://raw.githubusercontent.com/boduyskibidi/toi-yeu-ban-rat-nhieu/main/source.js
+// ==/UserScript==
