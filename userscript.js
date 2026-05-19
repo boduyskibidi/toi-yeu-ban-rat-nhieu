4f336037f3fa31cac42f4cb1eb10bbbd624c9c3c
boduyskibidi <duyminicpx@gmail.com>
2026-05-19 08:48:48 +0700

    Delete userscript.js

--- userscript.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         ANAKUxLOVANLEO
-// @namespace    ANAKU
-// @version      1.0
-// @match        *://linkhuongdan.online/*
-// @grant        none
-// @run-at       document-idle
-// @require      https://raw.githubusercontent.com/boduyskibidi/toi-yeu-ban-rat-nhieu/main/source.js
-// ==/UserScript==
